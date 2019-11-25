fe8f677b9fa21d57ba451d7f4b595efb39fc5499
super-model-4696 <jtb6179@g.rit.edu>
2019-11-25 04:37:26 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  addition = num1 + num2
+  return addition
 end
 
 def subtraction(num1, num2)
-
+  subtraction = num2 - num1
+  return subtraction
 end
 
 def division(num1, num2)
-
+  division = num2/num1
+  return division
 end
 
 def multiplication(num1, num2)
-
+  multiplication = num1*num2
+  return multiplication
 end
 
 def modulo(num1, num2)
-
+  modulo = num2%num1
+  return modulo
 end
 
 def square_root(num)
+  
 
 end
